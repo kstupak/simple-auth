041876bba4b92c187821961860216f46eee8a759
kstupak <konstantin.stupak@gmail.com>
2019-02-13 11:24:49 +0100

    let's try with bundles.

---
+++ src/SimpleAuthBundle.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * This file is part of "simple-auth".
+ *
+ * (c) Kostiantyn Stupak <konstantin.stupak@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace SimpleAuth;
+
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+final class SimpleAuthBundle extends Bundle
+{
+
+}
